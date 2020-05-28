83bdd76c8ef8fcb22eb2f5d24e12c2768a1f6c67
Adrien Guéret <adrien.grt@gmail.com>
2020-05-28 22:03:30 +0200

    refactor(app): use CssBaseline

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { ThemeProvider, makeStyles } from '@material-ui/core';
+import { ThemeProvider, CssBaseline } from '@material-ui/core';
 
 import { clientApi } from 'modules/app';
 import theme from 'modules/themes';
 import { CurrentLocaleProvider, setCurrentLocale, getUserLocale } from 'modules/i18n';
 import { CurrentUserProvider } from 'modules/users';
-
-const useStyles = makeStyles(() => ({
-    '@global': {
-        'body,html': {
-            padding: 0,
-            margin: 0,
-            minWidth: 320,
-        },
-    },
-}));
 
 if (typeof window !== 'undefined') {
     window.theme = theme;
@@ -31,10 +21,9 @@ function YathApp({ Component, pageProps, defaultCurrentLocale }) {
         clientApi.requestToken('client_credentials').then(() => setIsClientApiReady(true));
     }, [isClientApiReady]);
 
-    useStyles();
-
     return (
         <ThemeProvider theme={theme}>
+            <CssBaseline />
             <CurrentLocaleProvider defaultLocale={defaultCurrentLocale}>
                 <CurrentUserProvider>
                     <Component {...pageProps} />
